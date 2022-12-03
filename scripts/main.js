5d229c4733a5e35204939cfbc4b9b3092c022648
Rumeli Paul <rumelipaul@gmail.com>
2022-12-03 16:39:20 +0100

    implemented multiple obstacle creation and movements of obstacles

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-// create a class Player
+////////////////// Create a class Player //////////////////////////////////////////////////////////
 
 class Player {
   constructor() {
@@ -7,7 +7,7 @@ class Player {
     this.positionY = 40 - this.width / 2; // initial position is centered on Y axis.
     this.positionX = 0;
     this.domElement = null;
-     this.createDomElement(); // create player dynamically
+    this.createDomElement(); // create player dynamically
   }
 
   createDomElement() {
@@ -21,7 +21,7 @@ class Player {
     boardElm.appendChild(this.domElement);
   }
 
-  /////////////// Moving Player Up or Down
+  //>>>>>>>>>>>>> Moving Player Up or Down <<<<<<<<<<<<<<<<<<<<<<<<<<<<<//
 
   moveUp() {
     if (this.positionY > 10) {
@@ -37,9 +37,41 @@ class Player {
     }
   }
 }
+
+////////////////////////// Create Obstacle class ///////////////////////////////////
+class Obstacle {
+  constructor(randomPosition) {
+    this.width = 10;
+    this.height = 20;
+    this.positionY = randomPosition; // fix positions for obstacles randomly
+    this.positionX = 85;
+    this.domElement = null;
+    this.createDomElement(); // create obstacle dynamically
+  }
+
+  createDomElement() {
+    this.domElement = document.createElement("div");
+    this.domElement.id = "obstacle";
+    this.domElement.style.width = this.width + "vw";
+    this.domElement.style.height = this.height + "vh";
+    this.domElement.style.top = this.positionY + "vh";
+    this.domElement.style.left = this.positionX + "vw";
+    const boardElm = document.getElementById("board");
+    boardElm.appendChild(this.domElement);
+  }
+  moveObstacles() {
+    this.positionX -= 5;
+    this.domElement.style.left = this.positionX + "vw";
+    console.log(this.positionX);
+  }
+}
+
 //////////////////////// Declare global variables and create instance of Player.//////////////////////////////////
 
 const player = new Player();
+
+let bludgerArr = [];
+let snitchArr = [];
 
 //////////////////////// Move Player Up or Down /////////////////////////////////////////////////////////////////
 
@@ -51,3 +83,25 @@ document.addEventListener("keydown", (event) => {
     player.moveDown();
   }
 });
+
+///////////////////////// Move Obstacles /////////////////////////////////////////////////////////////////////////
+
+setInterval(() => {
+  bludgerArr.forEach((element) => {
+    element.moveObstacles();
+  });
+}, 500);
+
+//////////////////////// Creating multiple Bludgers randomly /////////////////////////////////////////////////////////////
+
+setInterval(() => {
+  generateBludgers();
+}, 2000);
+//////////////////// Generate positions for multiple bludgers //////////////////////////////////////////////////////////
+
+function generateBludgers() {
+  let positionsArr = [10, 30, 50]; // creating array for bludger positions so that collision can happen.
+  let item = positionsArr[Math.floor(Math.random() * positionsArr.length)];
+  const newBludger = new Obstacle(item);
+  bludgerArr.push(newBludger);
+}
