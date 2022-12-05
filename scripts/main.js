5404e2021a889e04b4909ed709d7232059783991
Rumeli Paul <rumelipaul@gmail.com>
2022-12-05 15:57:39 +0100

    refactored code and created game class

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,118 @@
+//////////////////////////////// Create game class /////////////////////////////////////////////////////
+
+class Game {
+    constructor(){
+        this.player = null;
+        this.bludgerArr = [];
+        this.snitchArr = [];
+        this.positionsArr = [10, 30, 50]; 
+        this.frame = 0;
+        this.score = 0;
+    }
+    start() {
+        this.player = new Player(); // player created
+        this.score = new Score();
+        this.attachEventlisteners();
+         
+          setInterval(() => {
+            this.frame += 1;
+            generateBludgers();
+            collisionDetection(this.bludgerArr);
+          }, 500);
+
+          const generateBludgers = () => {
+           // creating array for bludger positions so that collision can happen.
+            let item = this.positionsArr[Math.floor(Math.random() * this.positionsArr.length)];
+            if (this.frame % 4 === 0) {
+              const newBludger = new Obstacle(item, 20);
+              console.log(this.bludgerArr);
+              this.bludgerArr.push(newBludger);
+            }
+          }
+          const intervalid1 = setTimeout(() => {
+            setInterval(() => {
+              if (this.frame % 60 === 0 && this.snitchArr.length < 1) {
+                generateSnitches(); // generate snitch only one at a time in the viewport.
+              }
+              collisionDetection(this.snitchArr);
+            }, 300);
+          }, 30000);
+
+          const generateSnitches = () => {
+            // creating array for snitch positions so that collision can happen.
+            let index = this.positionsArr[Math.floor(Math.random() * this.positionsArr.length)];
+            if (index === this.player.positionY) {
+              switch (index) {
+                case 10:
+                  index = index * 2;
+                  break;
+                case 20:
+                  index = index / 2;
+                  break;
+                case 30:
+                  index = index / 3;
+                  break;
+              }
+            }
+          
+             const newSnitch = new Snitch(index, 10);
+            this.snitchArr.push(newSnitch);
+          }
+
+          const collisionDetection =(obstacleArr) => {
+            obstacleArr.forEach((element) => {
+              let typeOfObstacle = element.domElement.id;
+              element.moveObstacles(); // Moving bludgers or snitches
+              if (
+                this.player.positionX + this.player.width > element.positionX &&
+                this.player.positionY === element.positionY
+              ) {
+                if (typeOfObstacle === "obstacle") {
+                  gameOver();
+                } else if (typeOfObstacle === "snitch") {
+                  this.score.currentScore += 1000;
+                  this.score.update();
+                  this.score.collectedSnitch += 1;
+                  this.score.collectSnitch();
+                  element.domElement.remove();
+                obstacleArr.shift();
+                }
+              }
+              ///check if we need to remove current obstacle
+              if (element.positionX <= 0 - element.width) {
+                if (typeOfObstacle === "obstacle") {
+                  this.score.currentScore += 100;
+                  this.score.update();
+                }
+                element.domElement.remove();
+                obstacleArr.shift(); // remove the obstacle instance from the array as well.
+              }
+            });
+          
+            
+          }
+
+          const gameOver = () =>  {
+            localStorage.setItem("finalScore", this.score.currentScore.toString());
+            localStorage.setItem("numberOfSnitches", this.score.collectedSnitch.toString());
+            location.href = "../gameover.html";
+            
+          }
+
+    }
+    
+    attachEventlisteners(){                                           // On arrow key press move player.
+        document.addEventListener("keydown", (event) => {
+            // "ArrowUp" or "ArrowDown"
+            if (event.key === "ArrowUp") {
+              this.player.moveUp();
+            } else if (event.key === "ArrowDown") {
+              this.player.moveDown();
+            }
+          });
+    }
+}
+
 ////////////////// Create a class Player ///////////////////////////////////////////////////////////////
 
 class Player {
@@ -101,109 +216,14 @@ class Score {
 
 //////////////////////// Declare global variables and create instance of Player./////////////////////////
 
-const player = new Player();
-let score = new Score();
-let frame = 0;
-let bludgerArr = [];
-let snitchArr = [];
 
-//////////////////////// Move Player Up or Down ////////////////////////////////////////////////////////
+const game = new Game();
+game.start();
 
-document.addEventListener("keydown", (event) => {
-  // "ArrowUp" or "ArrowDown"
-  if (event.key === "ArrowUp") {
-    player.moveUp();
-  } else if (event.key === "ArrowDown") {
-    player.moveDown();
-  }
-});
-//////////////////////// Creating multiple Bludgers and Snitches randomly and moving them //////////////
 
-setInterval(() => {
-  frame++;
-  generateBludgers();
 
-  collisionDetection(bludgerArr);
-}, 500);
-//////////////////// Generate positions for multiple bludgers ////////////////////////////////////////
 
-function generateBludgers() {
-  let positionsArr = [10, 30, 50]; // creating array for bludger positions so that collision can happen.
-  let item = positionsArr[Math.floor(Math.random() * positionsArr.length)];
-  if (frame % 4 === 0) {
-    const newBludger = new Obstacle(item, 20);
-    bludgerArr.push(newBludger);
-  }
-}
 
-///////////////// Generate positions for snitches ////////////////////////////////////////
 
-function generateSnitches() {
-  let positionsArr = [10, 30, 50]; // creating array for snitch positions so that collision can happen.
-  let index = positionsArr[Math.floor(Math.random() * positionsArr.length)];
-  if (index === player.positionY) {
-    switch (index) {
-      case 10:
-        index = index * 2;
-        break;
-      case 20:
-        index = index / 2;
-        break;
-      case 30:
-        index = index / 3;
-        break;
-    }
-  }
 
-  const newSnitch = new Snitch(index, 10);
-  snitchArr.push(newSnitch);
-}
 
-//////////////// Creating Multiple snitches, and moving them ///////////////////////////////////////////////
-
-const intervalid1 = setTimeout(() => {
-  setInterval(() => {
-    if (frame % 60 === 0 && snitchArr.length < 1) {
-      generateSnitches(); // generate snitch only one at a time in the viewport.
-    }
-    collisionDetection(snitchArr);
-  }, 300);
-}, 30000);
-
-function collisionDetection(obstacleArr) {
-  obstacleArr.forEach((element) => {
-    let typeOfObstacle = element.domElement.id;
-    element.moveObstacles(); // Moving bludgers or snitches
-    if (
-      player.positionX + player.width > element.positionX &&
-      player.positionY === element.positionY
-    ) {
-      if (typeOfObstacle === "obstacle") {
-        gameOver();
-      } else if (typeOfObstacle === "snitch") {
-        score.currentScore += 1000;
-        score.update();
-        score.collectedSnitch += 1;
-        score.collectSnitch();
-        element.domElement.remove();
-      obstacleArr.shift();
-      }
-    }
-    ///check if we need to remove current obstacle
-    if (element.positionX <= 0 - element.width) {
-      if (typeOfObstacle === "obstacle") {
-        score.currentScore += 100;
-        score.update();
-      }
-      element.domElement.remove();
-      obstacleArr.shift(); // remove the obstacle instance from the array as well.
-    }
-  });
-
-  function gameOver() {
-    localStorage.setItem("finalScore", score.currentScore.toString());
-    localStorage.setItem("numberOfSnitches", score.collectedSnitch.toString());
-    location.href = "../gameover.html";
-    
-  }
-}
